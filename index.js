4c42967811203b7587955ecce489331491894ec6
Anupam Dutta Roy <anupam.capm22@gmail.com>
2022-08-18 07:23:10 +0000

    Updated to handle status query from chatbot

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const workflow = require('./workflow');
+const validate = require('uuid-validate');
 
 const dotenv = require('dotenv');
 dotenv.config()
@@ -15,7 +16,7 @@ app.post('/welcome', (req, res) => {
     res.send({
         replies: [{
             type: 'text',
-            content: "Hi ADR" 
+            content: "Hi ADR"
         }],
         conversation: {
             memory: {
@@ -26,44 +27,123 @@ app.post('/welcome', (req, res) => {
     });
 });
 
-app.post('/createso', (req, res) => {
-    let productID = req.body.conversation.memory.v_prodid.raw;
-    let customerID = req.body.conversation.memory.userID;
-    let index = req.body.conversation.memory.prod_disp.findIndex(el => el.ID == productID);
-    // console.log(index);
-    // console.log(req.body.conversation.memory.prod_disp);
-    let unitPrice = req.body.conversation.memory.prod_disp[index].price;
 
+app.post('/sostatus', (req, res) => {
     
+
+    // console.log(validate('49498b38-0202-45e3-ae77-9426213bf101'));
+    // console.log(validate('d411bd9f-6134-456d-b312-c78b14ff0bfd'));
+    // return;
+    let ordID = req.body.conversation.memory.v_ordid.raw;
+    // const regexExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+    // console.log(regexExp.test(ordID));
+
+    if (validate(ordID)) {
+        const option = {
+            'method': 'GET',
+            'url': 'https://469b9a7atrial-dev-wfsostaging-srv.cfapps.us10.hana.ondemand.com/catalog/OrderStatus(ID=' + ordID + ')',
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': JSON.stringify({
+                'ID': ordID, //'d411bd9f-6134-456d-b312-c78b14ff0bfd',
+            })
+        };
+
+        request(option, (error, response) => {
+            if (error) throw new Error(error);
+            console.log(response.body);
+            let conv_out = req.body.conversation;
+            conv_out.memory.statusText = JSON.parse(response.body).value.text;
+            res.send({
+                replies: [{
+                    type: 'text',
+                    content: "Status Received"
+                }],
+                conversation: conv_out
+            });
+
+        });
+    } else {
+        let conv_out_fail = req.body.conversation;
+        conv_out_fail.memory.statusText = ordID + ' is not valid.';
+        res.send({
+            replies: [{
+                type: 'text',
+                content: "Status Received"
+            }],
+            conversation: conv_out_fail
+        });
+    }
+
+});
+
+app.post('/createso', (req, res) => {
+    // console.log('data' + req.data);
+    // console.log('-------------');
+    console.log(req.body);
+    console.log(typeof (req.body.conversation));
+    let productID = '';
+    let customerID = '';
+    let quantity = 0;
+    let unitPrice = 0;
+    if (typeof (req.body.conversation) !== 'undefined') {
+        // if (1 == 2) {    
+        productID = req.body.conversation.memory.v_prodid.raw;
+        customerID = req.body.conversation.memory.userID;
+
+        if (req.body.conversation.memory.quantity == null) {
+            quantity = 1;
+        } else {
+            quantity = req.body.conversation.memory.quantity;
+        }
+        let index = req.body.conversation.memory.prod_disp.findIndex(el => el.ID == productID);
+        // console.log(index);
+        // console.log(req.body.conversation.memory.prod_disp);
+        unitPrice = req.body.conversation.memory.prod_disp[index].price;
+    } else {
+        productID = req.body.productID;
+        customerID = req.body.customerID;
+        quantity = req.body.quantity;
+        unitPrice = req.body.price;
+    }
+
     var options = {
-    'method': 'POST',
-    'url': 'https://469b9a7atrial-dev-wfsostaging-srv.cfapps.us10.hana.ondemand.com/catalog/SalesOrders',
-    'headers': {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        "customerID": customerID,
-        "productID": productID,
-        "quantity": 1,
-        "unitPrice": unitPrice
-    })
+        'method': 'POST',
+        'url': 'https://469b9a7atrial-dev-wfsostaging-srv.cfapps.us10.hana.ondemand.com/catalog/SalesOrders',
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "customerID": customerID,
+            "productID": productID,
+            "quantity": quantity,
+            "unitPrice": unitPrice
+        })
 
     };
     request(options, function (error, response) {
         if (error) throw new Error(error);
         //console.log(response);
         orderID = response.body.ID;
-        res.send({
-            replies: [{
-                type: 'text',
-                content: "Order created" 
-            }],
-            conversation: {
-                memory: {
-                    orderID: JSON.parse(response.body).ID
+
+        if (typeof (req.body.conversation) !== 'undefined') {
+            res.send({
+                replies: [{
+                    type: 'text',
+                    content: "Order created"
+                }],
+                conversation: {
+                    memory: {
+                        orderID: JSON.parse(response.body).ID
+                    }
                 }
-            }
-        });
+            });
+        } else {
+            res.send({
+                id: JSON.parse(response.body).ID
+            });
+        }
 
         var context_data = JSON.stringify({
             "definitionId": "wfso_execute",
@@ -78,12 +158,12 @@ app.post('/createso', (req, res) => {
         workflow.trigger_wf(context_data);
     });
 
-    
+
 });
 
 app.get('/test', (req, res) => {
     console.log(process.env.HANACRED);
-    res.send(process.env.HANACRED+' '+process.env.TEST);
+    res.send(process.env.HANACRED + ' ' + process.env.TEST);
 });
 app.listen(port, () => {
     console.log('Server is running on port ', port);
